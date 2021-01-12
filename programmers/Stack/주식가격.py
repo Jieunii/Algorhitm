191f574ef509ca2960765558c51694f1295ac313
Jieunii <yjelbf@naver.com>
2021-01-12 21:26:12 +0900

    Solve a question

---
+++ programmers/Stack/주식가격.py
@@ -0,0 +1,9 @@
+def solution(prices):
+    length = len(prices)
+    answer = [0]*length
+    for i in range(length-1):
+        for j in range(i+1, length):
+            answer[i] += 1
+            if prices[i] > prices[j]:
+                break
+    return answer
